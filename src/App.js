97e27929318a76ba7cf91ad98913b06844a2c2e2
nzha7124@uni.sydney.edu.au <nzha7124@uni.sydney.edu.au>
2021-07-10 16:01:26 +1000

    separated tictactoe from games

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import './App.css';
+import TicTacToe from './TicTacToe.js';
+
+
+
+function App() {
+  return (
+    <>
+
+    {/* Router contains all of the routes for the pages/paths */}
+    <Router>      
+      <Switch>
+
+        <Route path='/games/tictactoe/' exact component={TicTacToe}/>
+
+      </Switch>
+
+    </Router>
+    </>
+  );
+}
+
+export default App;
